67e7f3739b13ffb069c2e503ac9d749f336ab1d2
Athieswari <aathirajakumar03@gmail.com>
2015-04-23 15:26:28 +0530

    remove files

--- files/WordFreq.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-def Word_freq():
-	fpath = open('/home/prodapt/Documents/documentation of tasks.txt')
-	print Counter(fpath.read().split()).most_common()
-
-Word_freq()
